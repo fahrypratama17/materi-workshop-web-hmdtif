abdd42003251f08a319c1a149b52830cc98b23fd
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-05-20 17:17:06 +0700

    feat: enhance Card component layout and styling for improved user interaction

--- src/feature/components/Card.tsx
+++ src/feature/components/Card.tsx
@@ -1,3 +1,5 @@
+import { MoveRight } from "lucide-react";
+
 type CardProps = {
   id: number;
   icon: string;
@@ -8,16 +10,23 @@ type CardProps = {
 
 const Card = ({ icon, category, title, desc }: CardProps) => {
   return (
-    <div className="cursor-pointer rounded-2xl border border-black/20 p-8 duration-300 hover:-translate-y-2">
+    <div
+      className={`flex cursor-pointer flex-col items-start justify-between space-y-4 rounded-2xl border border-black/20 p-8 duration-300 hover:-translate-y-2 ${category === "HTML" ? "hover:border-orange-600" : "hover:border-blue-700"}`}
+    >
       <img src={icon} alt={title} className="mb-4 h-16 w-16" />
       <p
-        className={`${category === "HTML" ? "text-orange-600" : "text-blue-700"}`}
+        className={`rounded-2xl px-2 font-bold ${category === "HTML" ? "bg-orange-500/20 text-orange-600" : "bg-blue-500/20 text-blue-700"}`}
       >
         {category}
       </p>
-      <h1>Title</h1>
+      <h1 className="font-bold">{title}</h1>
       <p>{desc}</p>
-      <button>Mulai Belajar</button>
+      <button
+        className={`flex items-center gap-2 ${category === "HTML" ? "text-orange-600" : "text-blue-700"}`}
+      >
+        Mulai Belajar
+        <MoveRight className="h-4 w-3" />
+      </button>
     </div>
   );
 };
