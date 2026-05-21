94773f9ec0d0453abf38d45443540e10ba9f22e6
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-05-21 16:31:14 +0700

    feat: add Materi component and integrate it into PengenalanHTML page

---
+++ src/features/pengenalan-html/components/Materi.tsx
@@ -0,0 +1,13 @@
+const Materi = () => {
+  return (
+    <section className="px-8">
+      <p>
+        HTML adalah singkatan dari HyperText Markup Language. HTML bukan bahasa
+        pemrograman, melainkan bahasa markup yang digunakan untuk mendefinisikan
+        struktur konten di halaman web.
+      </p>
+    </section>
+  );
+};
+
+export default Materi;
--- src/features/pengenalan-html/pages/PengenalanHTML.tsx
+++ src/features/pengenalan-html/pages/PengenalanHTML.tsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header.tsx";
+import Materi from "../components/Materi.tsx";
 
 const PengenalanHTML = () => {
   return (
     <>
       <Header />
+      <main>
+        <Materi />
+      </main>
     </>
   );
 };
