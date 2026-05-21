ec8f3bdd2494e7322d25ca64a3411774fbd739e9
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-05-21 11:33:59 +0700

    feat: add Header, Card, Filter, and Home components; integrate routing in main application

---
+++ src/features/home/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header.tsx";
+import Materi from "../components/Materi.tsx";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Materi />
+      </main>
+    </>
+  );
+};
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
-import "./style/index.css";
+import { StrictMode } from "react";
 import App from "./App.tsx";
+import "./style/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
