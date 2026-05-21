c553ed774e9fcbd0d4efe60e11b74f7496e8aef9
Muhamad Fahry Pratama Putra <m.fahry.pratama.putra@gmail.com>
2026-05-21 16:29:04 +0700

    feat: enhance Header component styling for improved visual hierarchy and readability

--- src/features/pengenalan-html/components/Header.tsx
+++ src/features/pengenalan-html/components/Header.tsx
@@ -17,14 +17,16 @@ const Header = () => {
       {MateriData.map((item) => (
         <div
           key={item.id}
-          className="flex flex-col gap-4 rounded-2xl border p-8"
+          className="flex flex-col gap-4 rounded-2xl border border-orange-600 bg-orange-500/20 p-8"
         >
-          <p className="font-bold">{item.category}</p>
+          <p className="w-fit rounded-2xl border border-orange-500 bg-white px-2 font-bold text-orange-600">
+            {item.category}
+          </p>
           <div className="flex items-center">
             <img src={item.icon} alt={item.title} className="h-16 w-16" />
             <h1 className="text-4xl font-bold">{item.title}</h1>
           </div>
-          <p>{item.desc}</p>
+          <p className="text-black/50">{item.desc}</p>
         </div>
       ))}
     </section>
